b1f4cbc3056dfbb8eae598bae165cf6fa0f20fce
Tobi <c4rgon4t@gmail.com>
2017-10-07 18:54:53 +0200

    Some Stuff for Decks with JFileChooser

--- src/Generator.java
+++ src/Generator.java
@@ -1,8 +1,15 @@
 import javax.swing.*;
+import javax.swing.filechooser.FileNameExtensionFilter;
 import java.awt.*;
 import java.awt.datatransfer.Clipboard;
 import java.awt.datatransfer.StringSelection;
 import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.List;
+
 import com.jtattoo.plaf.hifi.*;
 
 public class Generator
@@ -10,6 +17,9 @@ public class Generator
 
     private static GeneratorUI _generatorUI;
     private static MenuUI _menuUI;
+
+    private static File _cardsFolder = new File("cards");
+    private static File _decksFolder = new File("decks");
 
     private static void runMenu()
     {
@@ -33,13 +43,6 @@ public class Generator
     {
         _generatorUI = new GeneratorUI();
 
-        File cardFolder = new File("");
-        //Test if Folder exists, else make it
-        if (!cardFolder.exists())
-            cardFolder.mkdir();
-
-
-
         _generatorUI.getOutputJSONButton().addActionListener
                 (
                         ae ->
@@ -56,13 +59,51 @@ public class Generator
 
     private static void runDeckCompiler()
     {
+        JFileChooser chooser = new JFileChooser(_cardsFolder);
+        chooser.setMultiSelectionEnabled(true);
+        int returnValueOpener = chooser.showOpenDialog(null);
+        if (returnValueOpener == JFileChooser.APPROVE_OPTION)
+        {
+            File[] files = chooser.getSelectedFiles();
+            String json = "[\n";
+            for (File file : files)
+            {
+                String content = "";
+                try
+                {
+                    List<String> contentLines = Files.readAllLines(file.toPath());
+                    for (String line : contentLines)
+                        content += line + "\n";
+                    //cutoff square brackets and corresponding \n and last \n
+                    content = content.substring(3,content.length()-3) + ",\n";
+                } catch (IOException e)
+                {
+                    e.printStackTrace();
+                }
+                json += content;
+            }
+            json += "]";
 
+            JFileChooser saver = new JFileChooser(_decksFolder);
+            saver.setFileFilter(new FileNameExtensionFilter("JSON",".json"));
+            int returnValueSaver = saver.showSaveDialog(null);
+            File deck;
+            if (returnValueSaver == JFileChooser.APPROVE_OPTION)
+                deck = saver.getSelectedFile();
+            System.out.println(json);
+        }
     }
 
     public static void main(String[] args)
     {
-        //UIManager.setLookAndFeel("com.jtattoo.plaf.hifi.HiFiLookAndFeel");
-        //setDarkLookAndFeel();
+        //Test if Cards Folder exists, else make it
+        if (!_cardsFolder.exists())
+            _cardsFolder.mkdir();
+        //Test if Decks Folder exists, else make it
+        if (!_decksFolder.exists())
+            _decksFolder.mkdir();
+
+        //open menu
         runMenu();
     }
 }
